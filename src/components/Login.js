fb9f39fae2da0b938d153ee618ba1be05745b5b4
Harshit Mathur <harshitmathur@Harshits-MacBook-Air.local>
2022-04-04 20:52:54 +0530

    Modified login and signup pages

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import { useNavigate } from 'react-router-dom';
+import "./css/loginStyle.css";
 
 const Login = (props) => {
 
@@ -31,18 +32,29 @@ const Login = (props) => {
         setcreds({ ...creds, [e.target.name]: e.target.value })
     }
   return (
-    <div className='container my-2'>
-          <form onSubmit={handleSubmit}>
-              <div className="mb-3">
-                  <label htmlFor="email" className="form-label">Email address</label>
-                  <input type="email" className="form-control" value={creds.email} id="email" name="email" onChange={onChange} required/>
+    <div className='container'>
+        <h1 className="text-center my-3 py-3">Login to begin with NoteKeeper</h1>
+          <div className="col-12 col-sm-8 col-md-6 m-auto">
+              <div className="card border-0 shadow-lg">
+                  <div className="card-body">
+                      <form className="lform" onSubmit={handleSubmit}>
+                          <div className="text-center my-3">
+                              <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" className="bi bi-person-circle" viewBox="0 0 16 16">
+                                  <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                                  <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
+                              </svg>
+                          </div>
+                          <div className="my-2 py-2">
+                              <input type="email" className="form-control my-3 py-2" value={creds.email} id="email" name="email" onChange={onChange} placeholder="Email Address" required />
+                              <input type="password" className="form-control my-3 py-2" value={creds.password} id="password" name="password" onChange={onChange} placeholder="Password" minLength={8} required />
+                          </div>
+                          <div className="text-center mt-2">
+                              <button type="submit" className="btn btn-primary">Login</button>
+                          </div>
+                      </form>
+                  </div>
               </div>
-              <div className="mb-3">
-                  <label htmlFor="password" className="form-label">Password</label>
-                  <input type="password" className="form-control" value={creds.password} id="password" name="password" onChange={onChange} minLength={8} required/>
-              </div>
-              <button type="submit" className="btn btn-primary">Submit</button>
-          </form>
+          </div>
     </div>
   )
 }
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -30,26 +30,41 @@ const Signup = (props) => {
     setcreds({ ...creds, [e.target.name]: e.target.value })
   }
   return (
-    <div className='container my-2'>
-      <form onSubmit={handleSubmit}>
-        <div className="mb-3">
-          <label htmlFor="name" className="form-label">Name</label>
-          <input type="text" className="form-control" id="name" value={creds.name} onChange={onChange} name="name"/>
+    <div className='container'>
+      <h1 className="text-center my-3 py-3">Register to begin with NoteKeeper</h1>
+      <div className="col-12 col-sm-8 col-md-6 m-auto">
+        <div className="card border-0 shadow-lg">
+          <div className="card-body">
+            <form onSubmit={handleSubmit}>
+              <div className="text-center my-3">
+                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" className="bi bi-person-circle" viewBox="0 0 16 16">
+                  <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                  <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
+                </svg>
+              </div>
+              <div className="row mb-3">
+                <div className="col-12 col-md-6">
+                  <input type="text" className="form-control" id="name" value={creds.name} onChange={onChange} name="name" placeholder="Name" />
+                </div>
+                <div className="col-12 col-md-6">
+                  <input type="email" className="form-control" id="email" value={creds.email} onChange={onChange} name="email" placeholder="Email Address" />
+                </div>
+              </div>
+              <div className="row mb-3">
+                <div className="col-12 col-md-6">
+                  <input type="password" className="form-control" id="password" value={creds.password} onChange={onChange} name="password" placeholder="Password" minLength={8} required />
+                </div>
+                <div className="col-12 col-md-6">
+                  <input type="password" className="form-control" id="cpassword" value={creds.cpassword} onChange={onChange} name="cpassword" placeholder="Confirm Password" minLength={8} required />
+                </div>
+              </div>
+              <div className="text-center mt-2">
+                <button type="submit" className="btn btn-primary">Sign Up</button>
+              </div>
+            </form>
+          </div>
         </div>
-        <div className="mb-3">
-          <label htmlFor="email" className="form-label">Email address</label>
-          <input type="email" className="form-control" id="email" value={creds.email} onChange={onChange} name="email"/>
-        </div>
-        <div className="mb-3">
-          <label htmlFor="password" className="form-label">Password</label>
-          <input type="password" className="form-control" id="password" value={creds.password} onChange={onChange} name="password" minLength={8} required/>
-        </div>
-        <div className="mb-3">
-          <label htmlFor="cpassword" className="form-label">Confirm Password</label>
-          <input type="password" className="form-control" id="cpassword" value={creds.cpassword} onChange={onChange} name="cpassword" minLength={8} required/>
-        </div>
-        <button type="submit" className="btn btn-primary">Submit</button>
-      </form>
+      </div>
     </div>
   )
 }
